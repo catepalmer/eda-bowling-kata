25527995da5e8b7243d810f9fb15e1295261d78d
Cate Palmer <catherinepalmer@gmail.com>
2018-08-14 13:01:54 +1200

    Started working on basic structure of bowling solution

--- game.js
+++ game.js
@@ -9,3 +9,13 @@
 // var frames = [
 //   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
 // ]
+
+
+function addScore (frames) {
+
+}
+
+
+function scoreFrame (frame) {
+    
+}
